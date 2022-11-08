98a82b3cb76b8605a8f4fb74a15e2b866b4e77da
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-11-08 14:20:26 +0300

    project progres

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import { useRef } from "react";
+import {FaBars,FaTimes} from "react-icons/fa";
+ 
+
+function Navbar(){
+    const navRef = useRef();
+
+    const showNavbar = ()=> {
+        navRef.current.classList.toggle("responsive-nav");
+    }
+    return(
+    <header>
+        <h3>LOGO</h3>
+       <nav ref={navRef}>
+        <a href="/#">Home</a>
+        <a href="/#">About</a>
+        <a href="/#">Blog</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+            <FaTimes/>
+        </button>
+        </nav> 
+        <button className="nav-btn" onClick={showNavbar}>
+            <FaBars/>
+
+            </button>
+    </header> 
+    );
+}
+
+export default Navbar;

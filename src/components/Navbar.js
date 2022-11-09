ce39d973c8d47ad3064fd383c14747f2cb5450bc
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-11-09 16:25:10 +0300

    project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,6 @@ function Navbar(){
         <h3>LOGO</h3>
        <nav ref={navRef}>
         <a href="/#">Home</a>
-        <a href="/#">Apartment</a>
         <a href="/#">About</a>
         <a href="/#">Contact Us</a>
         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
